1315fdd86b52edb2669dfc901d3a5348266bdd10
run_global <1152016254@qq.com>
2019-06-16 13:23:28 +0800

    增加tslib使用的环境变量的导出脚本

---
+++ tools/ts_config.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+
+export TSLIB_TSDEVICE=/dev/input/event0
+export TSLIB_CONFFILE=/etc/ts.conf
+export TSLIB_PLUGINDIR=/lib/ts
+export TSLIB_CONSOLEDEVICE=none
+export TSLIB_FBDEVICE=/dev/fb0
+
