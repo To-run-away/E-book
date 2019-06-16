ff35242bfea884289f615d9f1f40bc2dea0ef750
run_global <1152016254@qq.com>
2019-06-16 13:24:16 +0800

    添加tslib使用到的输入设备路径

--- include/config.h
+++ include/config.h
@@ -5,6 +5,7 @@
 #include <stdio.h>
 
 #define FB_DEVICE_NAME "/dev/fb0"
+#define TOUCHSCREEN_DEVICE_NAME	"/dev/input/event0"
 
 #define COLOR_BACKGROUND   0xE7DBB5  /* 泛黄的纸 */
 #define COLOR_FOREGROUND   0x514438  /* 褐色字体 */
@@ -12,6 +13,14 @@
 #define DBG_PRINTF(...)  
 //#define DBG_PRINTF printf
 
+/*
+ * 输入方式分为三种：
+ * 1:轮询方式
+ * 2:select方式
+ * 3:多线程方式
+ * 
+ */
+#define INPUT_WAY 2	
 
 #endif /* __CONFIG_H__ */
 
